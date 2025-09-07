0db39ea6c5fa80a73a1c9ba949477f042609518a
Belkacem Messaoudi <139685303+Messaoudi-Belkacem@users.noreply.github.com>
2025-09-07 01:01:50 +0100

    Implement permission handling in MainActivity; adjust start destination based on permission status and API level

--- app/src/main/java/com/example/quranapp/MainActivity.kt
+++ app/src/main/java/com/example/quranapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
@@ -21,22 +22,40 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val tag = "MainActivity.kt"
-    private lateinit var navHostController: NavHostController
+    private val tag = MainActivity::class.java.simpleName
     private val sharedViewModel: SharedViewModel by viewModels()
+    private lateinit var navHostController: NavHostController
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            Log.d(tag, "Permission granted")
+            navHostController.navigate(Screen.HomeRoute.route) {
+                popUpTo(0) // Clear back stack
+            }
+        } else {
+            Log.d(tag, "Permission denied")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        sharedViewModel
-        val startDestination: String
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+        val startDestination: String = if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             Log.d(tag, "Permission is granted")
-            startDestination = Screen.HomeRoute.route
+            Screen.HomeRoute.route
         } else {
             Log.d(tag, "Permission is not granted")
-            startDestination = Screen.PermissionRoute.route
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            Screen.PermissionRoute.route
         }
+
+        val apiLevel = android.os.Build.VERSION.SDK_INT
+        val adjustedStartDestination = if (apiLevel >= 34 && startDestination == Screen.PermissionRoute.route) {
+            Screen.HomeRoute.route
+        } else {
+            startDestination
+        }
+
         setContent {
             AppTheme {
                 navHostController = rememberNavController()
@@ -44,7 +63,7 @@ class MainActivity : ComponentActivity() {
                     RootNavigationGraph(
                         innerPadding = innerPadding,
                         navHostController = navHostController,
-                        startDestination = startDestination
+                        startDestination = adjustedStartDestination
                     )
                 }
             }
